15de3e2a9b06df568999c52f8d497771d8fe3721
YaBoleyu <taras_demkiv@yahoo.com>
2021-12-09 23:14:24 +0300

    Commit to master 09.12.2021

---
+++ Stepik/Python/Модуль_3_Тестовые_фреймворки/3.5.PyTest_Маркировка/test_mark1.py
@@ -0,0 +1,26 @@
+import pytest
+from selenium import webdriver
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+
+class TestMainPage1():
+    #Маркирование тестов для того что бы удобнее собирать их в тест-планы @pytest.mark.* 
+    @pytest.mark.smoke
+    def test_guest_should_see_login_link(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector("#login_link")
+
+    @pytest.mark.regression
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector(".basket-mini .btn-group > a")
